355dde32df33b0f4fa24bba3b8697abe07a20795
Vipul Gupta <71564722+vipulgupta31@users.noreply.github.com>
2022-02-06 21:15:41 +0530

    Update DataProviderExample.java

--- src/test/DataProviderExample.java
+++ src/test/DataProviderExample.java
@@ -1,20 +1,42 @@
 package test;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class DataProviderExample {
 
-	@DataProvider(name = "first-data-provider")
-	public Object[][] dataProviderMethod()
+	@DataProvider(name = "browserName")
+	public Object[][] browserName() 
 	{
-	return new Object[][] {{"One"}, {"Two"}};
+		return new Object[][] { { "chrome" }, { "firefox" } };
 	}
 
-	@Test(dataProvider = "first-data-provider")
-	public void dataProviderTest(String value)
+	@Test(dataProvider = "browserName")
+	public void crossBrowserTest(String browser) 
 	{
-	System.out.println("Passed value is : " + value);
+		WebDriver driver = null;
+		
+		System.out.println("Launching browser : " + browser);
+		
+		switch(browser)
+		{
+			case "chrome":
+				System.setProperty("webdriver.chrome.driver", "<Path_to_your_chrome_driver>");
+				driver = new ChromeDriver();
+				break;
+			case "firefox":
+				System.setProperty("webdriver.firefox.driver", "<Path_to_your_firefox_driver>");
+				driver = new FirefoxDriver();
+				break;
+			default:
+				System.out.println("Invalid browser name passed");
+		}
+		
+		driver.navigate().to("http://www.lambdatest.com/");
+		System.out.println("Navigated Successfully to the website");
 	}
 
 }
